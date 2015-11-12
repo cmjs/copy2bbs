6c4cbf71f6c0d13e5b5552c39304b3c4a9457c94
liu <277153007@qq.com>
2015-11-12 19:16:15 +0800

    ai

--- water_machine.py
+++ water_machine.py
@@ -11,6 +11,9 @@ import urllib2
 
 content  = ("看看","试试","灌水","回帖加一","我能","再看看","路过一个的说","就是这样","西风来几日","一叶已先飞","新霁乘轻屐","初凉换熟衣","浅渠销慢水","疏竹漏斜晖","薄暮青苔巷","家僮引鹤归")
 
+
+last_page_url = "http://bbs.uestc.edu.cn/forum.php?mod=viewthread&tid=1559009&extra=&page=9999"
+
 if __name__ == '__main__':
     my_account  = discuz.Discuz()
     while True:
@@ -20,32 +23,21 @@ if __name__ == '__main__':
             print 'login failed'
         else:
             print 'login succeed'
-
+            lastreply_pattern = re.compile(r'div id="post_(\d+)".*?uid=(\d+)',re.S)
             while True:
                 try:
-                    home_response  = my_account._get_response(url=config.WATERURL).read()
-                    lastpage_pattern = re.compile(r'class="last">.*?(\d+)</a>',re.S)
-                    lastpage_search_result = lastpage_pattern.search(home_response)
-                    if lastpage_search_result:
-                        #print 'fetch lastpage_num succeed'
-                        last_page = lastpage_search_result.group(1)
-                        target_url= config.WATERURL + '&extra=&page=' + last_page
-                        target_url_response = my_account._get_response(url=target_url).read()
-
-                        #print target_url
-                        lastreply_pattern = re.compile(r'div id="post_(\d+)".*?uid=(\d+)',re.S)
-
-                        lastreply_result = lastreply_pattern.findall(target_url_response)
-                        if lastreply_result:
-                            if config.USERUID != lastreply_result[-1][-1]:
-                                print "can reply"
-                                my_account.reply(config.WATERURL,content[random.randint(0,15)])
-                                time.sleep(10)
-                                print "2 lian"
-                                my_account.reply(config.WATERURL,content[random.randint(0,15)])
-                            else:
-                                print "will 3 lian"
-                                time.sleep(10)
+                    target_url_response = my_account._get_response(url=last_page_url).read()
+                    lastreply_result = lastreply_pattern.findall(target_url_response)
+                    if lastreply_result:
+                        if config.USERUID != lastreply_result[-1][-1]:
+                            print "can reply"
+                            my_account.reply(config.WATERURL,content[random.randint(0,15)])
+                            time.sleep(10)
+                            print "2 lian"
+                            my_account.reply(config.WATERURL,content[random.randint(0,15)])
+                        else:
+                            print "will 3 lian"
+                            time.sleep(10)
                 except urllib2.HTTPError, e:
                     print 'except:', e
 
--- ww_machine.py
+++ ww_machine.py
@@ -11,50 +11,78 @@ import urllib2
 
 content  = ("看看","试试","灌水","回帖加一","我能","再看看","路过一个的说","就是这样","西风来几日","一叶已先飞","新霁乘轻屐","初凉换熟衣","浅渠销慢水","疏竹漏斜晖","薄暮青苔巷","家僮引鹤归")
 content2 = ("黑哥哥好厉害惹","抢不到啊","狙击狙击","快没水了","试试","水汽","再来","不要停")
-content3 = ("试试运气","水汽")
+content3 = ("555555","666666")
 content4 = "80L"
+
+last_page_url = "http://bbs.uestc.edu.cn/forum.php?mod=viewthread&tid=1570810&extra=&page=9999＆mobile=1"
+
 if __name__ == '__main__':
     my_account  = discuz.Discuz()
     while True:
         username = raw_input('Please input username:')
         password = raw_input('Please input password:')
+        mode     = raw_input('choose mode: 1->*8  2->80 08:')
         if not my_account.login(username, password):
             print 'login failed'
         else:
             print 'login succeed'
-
+            #lastreply_pattern = re.compile(r'div id="post_\d+".*?postnum.*?em>.*?(\d+)',re.S)
+            lastreply_pattern = re.compile(r'postnum.*?em>.*?(\d+)',re.S)
+            pattern_79  = re.compile(r'\d{3}79')
+            pattern_77  = re.compile(r'\d{3}77')
+            pattern_07  = re.compile(r'\d{3}07')
             while True:
                 try:
-                    home_response  = my_account._get_response(url=config.WWURL).read()
-                    lastpage_pattern = re.compile(r'class="last">.*?(\d+)</a>',re.S)
-                    lastpage_search_result = lastpage_pattern.search(home_response)
-                    if lastpage_search_result:
-                        #print 'fetch lastpage_num succeed'
-                        last_page = lastpage_search_result.group(1)
-                        target_url= config.WWURL + '&extra=&page=' + last_page
-                        target_url_response = my_account._get_response(url=target_url).read()
+                    target_url_response = my_account._get_response(url=last_page_url).read()
 
-                        #print target_url
-                        lastreply_pattern = re.compile(r'div id="post_\d+".*?postnum.*?em>.*?(\d+)',re.S)
 
-                        lastreply_result = lastreply_pattern.findall(target_url_response)
-                        #print lastreply_result
-                        if lastreply_result:
+
+                    #print target_url_response
+                    lastreply_result = lastreply_pattern.findall(target_url_response)
+                    #print lastreply_result
+
+
+                    if lastreply_result:
+                        if mode == '1':
                             if (len(lastreply_result) == 7):
                                 print "reply 8"
                                 my_account.reply(config.WWURL,content3[random.randint(0,1)])
-                                time.sleep(6)
-                            elif len(lastreply_result) == 17 and (not re.match(r'\d{3}77',lastreply_result[-1])) :
-                                print "reply 18"
-                                my_account.reply(config.WWURL,content3[random.randint(0,1)])
-                                time.sleep(6)
-                            elif re.match(r'\d{3}79',lastreply_result[-1]):
-                                print "reply 80"
-                                my_account.reply(config.WWURL,content4)
-                                time.sleep(6)
+                                time.sleep(5)
+                            elif (len(lastreply_result) == 19):
+                                #if re.match(r'\d{3}79',lastreply_result[-1]):
+                                if pattern_79.match(lastreply_result[-1]):
+                                    my_account.reply(config.WWURL,content3[random.randint(0,1)])
+                                    print "reply 80"
+                                else:
+                                    print "ignore"
+                                    time.sleep(5)
+                            elif len(lastreply_result) == 17:
+                                if (not pattern_77.match(lastreply_result[-1])) :
+                                    print "reply 18"
+                                    my_account.reply(config.WWURL,content3[random.randint(0,1)])
+                                    time.sleep(5)
+                                else:
+                                    print "77 ignore"
                             else:
                                 print len(lastreply_result)
-                                #time.sleep(6)
+                        elif mode == '2':
+                            if (len(lastreply_result) == 19):
+                                #if re.match(r'\d{3}79',lastreply_result[-1]):
+                                if pattern_79.match(lastreply_result[-1]):
+                                    my_account.reply(config.WWURL,content3[random.randint(0,1)])
+                                    print "reply 80"
+                                else:
+                                    print "ignore"
+                            elif (len(lastreply_result) == 7):
+                                #if re.match(r'\d{3}07',lastreply_result[-1]):
+                                if pattern_07.match(lastreply_result[-1]):
+                                    my_account.reply(config.WWURL,content3[random.randint(0,1)])
+                                    print "reply 08"
+                                else:
+                                    print "ignore"
+                            else:
+                                print len(lastreply_result)
+                            #time.sleep(6)
                 except urllib2.HTTPError, e:
                     print 'except:', e
 
